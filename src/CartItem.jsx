91b0bf1346d36ea4e5b0dab8e7f099a19ab84919
AserHub25 <AserHub25>
2025-06-11 17:53:41 -0400

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -8,22 +8,39 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
+const calculateTotalAmount = () => {
     let total = 0;
-    
- 
+    cart.forEach(item => {
+        const numCost = parseFloat(item.cost.toString().replace(/[^\d.]/g, ''));
+        total += numCost * item.quantity;
+    });
+    return total.toFixed(2);
   };
 
+
   const handleContinueShopping = (e) => {
+    if (onContinueShopping) {
+        onContinueShopping(e);
+    }
    
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+        name: item.name,
+        quantity: item.quantity + 1
+    }));
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({
+            name: item.name,
+            quantity: item.quantity - 1
+        }));
+    }
    
   };
 
@@ -32,6 +49,10 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
