02f2fab1e6c6ed170a934ed9f3efffe6035b4677
AserHub25 <AserHub25>
2025-06-19 05:17:47 -0400

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,63 +1,57 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice.jsx';
+import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
-    
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-    const calculateTotalAmount = () => {
-        let total = 0;
-            cart.forEach(item => {
-                const numCost = parseFloat(item.cost.toString().replace(/[^\d.]/g, ''));
-                    total += numCost * item.quantity;
-            });
+  const calculateTotalAmount = () => {
+    let total = 0;
+
+    cart.forEach((item) => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1));
+        total += quantity * cost;
+    }
+
+    );
     return total.toFixed(2);
   };
 
-
   const handleContinueShopping = (e) => {
-    if (onContinueShopping) {
-        onContinueShopping(e);
-    }
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({
-        name: item.name,
-        quantity: item.quantity + 1
-    }));
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
     if (item.quantity > 1) {
-        dispatch(updateQuantity({
-            name: item.name,
-            quantity: item.quantity - 1
-        }));
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+        dispatch(removeItem(item.name));
     }
-   
   };
 
   const handleRemove = (item) => {
+    
     dispatch(removeItem(item.name));
-  };
-
-  // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
-    const numCost = parseFloat(item.cost.toString().replace(/[^\d.]/g, ''));
-
-    return (numericCost * item.quantity).toFixed(2);
   };
 
   const handleCheckoutShopping = (e) => {
     alert('Functionality to be added for future reference');
+  };
+
+  // Calculate total cost based on quantity for an item
+  const calculateTotalCost = (item) => {
+    const price = parseFloat(item.cost.substring(1));
+    return (item.quantity * price).toFixed(2);
   };
 
   return (
@@ -92,5 +86,3 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
 export default CartItem;
-
-
